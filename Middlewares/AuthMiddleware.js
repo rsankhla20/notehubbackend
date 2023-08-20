60beb3b33bc8cd36018b3c5611b9d5d72b5e8121
Rakesh Sankhla <75526736+rsankhla20@users.noreply.github.com>
2023-08-21 03:43:20 +0530

    first commit of backend

---
+++ Middlewares/AuthMiddleware.js
@@ -0,0 +1,23 @@
+const User = require("../Models/UserModel");
+const Note = require("../Models/NotesModel");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+module.exports.userVerification = async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.json({ status: false });
+  }
+  jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
+    if (err) {
+      return res.json({ status: false });
+    } else {
+      const user = await User.findById(data.id);
+      const notes = await Note.find({ userId: user._id });
+      //console.log(user);
+      // console.log(notes);
+      if (user) return res.json({ status: true, user: user, note: notes });
+      else return res.json({ status: false });
+    }
+  });
+};
