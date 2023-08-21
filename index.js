c20916ca55218105761e55b9aee4db8a767c204a
Rakesh Sankhla <75526736+rsankhla20@users.noreply.github.com>
2023-08-21 19:35:19 +0530

    update

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
   );
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
 app.use(cookieParser());
